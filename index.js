5f7000353f356dbf46df15def47902b952b41079
Mattyus <matanoliel20@gmail.com>
2022-10-16 00:13:23 +0300

    added dev docker file

--- index.js
+++ index.js
@@ -17,5 +17,13 @@ const res = await new Promise((resolve, reject) => {
 console.log(res?.message);
 
 app.listen(port, () => {
-  console.log(`[Express] is listening on port: ${port}`);
+  console.log(`[Express] is listening on port [new]: ${port}`);
 });
+
+// example build command (linux):
+// docker build -t node:docker-dev -f Dockerfile.development .
+
+
+// example runs command (linux):
+// docker run -p 5000:5000 --name node-app -v "$(pwd)":/usr/src/app -v /usr/src/app/node_modules -it node:docker-dev
+// docker run -p 5000:5000 --name node-app -v "$(pwd)":/usr/src/app -v /usr/src/app/node_modules node:docker-dev
